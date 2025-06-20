ce761e30ab6f0a76539e170c60ddb159fa6754bd
Chaoyi Jiang <jiang.chao@northeastern.edu>
2025-06-19 23:28:55 -0400

    LC-valid-anagram [Runtime: 11 ms; Memory: 17.7 MB]

--- hash-table/valid-anagram.py
+++ hash-table/valid-anagram.py
@@ -3,4 +3,9 @@ class Solution:
         if len(s) != len(t):
             return False
 
-        return sorted(s) == sorted(t)
+        count = [0] * 26
+        for i in range(len(s)):
+            count[ord(s[i]) - ord('a')] += 1
+            count[ord(t[i]) - ord('a')] -= 1
+
+        return all(c == 0 for c in count)
