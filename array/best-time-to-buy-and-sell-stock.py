ff3d8e1ee96b81668ab3d8acf7c4d519eb03c0b4
Chaoyi Jiang <jiang.chao@northeastern.edu>
2025-06-19 23:20:54 -0400

    LC-best-time-to-buy-and-sell-stock [Runtime: 25 ms; Memory: 26.8 MB]

--- array/best-time-to-buy-and-sell-stock.py
+++ array/best-time-to-buy-and-sell-stock.py
@@ -1,5 +1,8 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        if not prices or len(prices) < 2:
+            return 0
+
         min_price = prices[0]
         max_profit = 0
 
