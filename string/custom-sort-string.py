7b1dd00a049de721458552d064ca441ae1096f65
Chaoyi Jiang <jiang.chao@northeastern.edu>
2025-08-24 16:55:01 -0400

    LC-custom-sort-string [Runtime: 0 ms; Memory: 18 MB]

--- string/custom-sort-string.py
+++ string/custom-sort-string.py
@@ -1,4 +1,14 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-        rank = {c: i for i, c in enumerate(order)}
-        return "".join(sorted(s, key=lambda ch:rank.get(ch, len(order))))
+        cnt =Counter(s)
+        ans = []
+
+        for c in order:
+            if cnt[c]:
+                ans.append(c * cnt[c])
+                del cnt[c]
+
+        for c, k in cnt.items():
+            ans.append(c * k)
+
+        return ''.join(ans)
