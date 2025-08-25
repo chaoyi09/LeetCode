fec4b257aba209ab978dcf9a42a9b1e6f0833ae3
Chaoyi Jiang <jiang.chao@northeastern.edu>
2025-08-24 20:35:51 -0400

    LC-basic-calculator-ii [Runtime: 63 ms; Memory: 18.9 MB]

--- math/basic-calculator-ii.py
+++ math/basic-calculator-ii.py
@@ -1,68 +1,30 @@
-from collections import deque
 
 class Solution:
     def calculate(self, s: str) -> int:
-        deNum = deque()
-        deOp  = deque()
+        s = s.replace(' ', '')
+        num = 0
+        last = 0
+        ans = 0
+        sign = '+'
 
-        def calc(a: int, b: int, op: str) -> int:
-            if op == '+': return a + b
-            if op == '-': return a - b
-            if op == '*': return a * b
-            if op == '/': return int(a / b)
-            return 0
+        for i, ch in enumerate(s):
+            if ch.isdigit():
+                num = num * 10 + ord(ch) - 48
+            if not ch.isdigit() or i == len(s) - 1:
+                if i == len(s) - 1 and ch.isdigit():
 
-        opFlag = 0
-        i, n = 0, len(s)
-
-        while i < n:
-            ch = s[i]
-            if ch in "*/":
-                opFlag = 1
-                deOp.append(ch)
-                i += 1
-            elif ch in "+-":
-                j = i - 1
-                while j >= 0 and s[j] == ' ':
-                    j -= 1
-                if j < 0 or s[j] in "+-*/":
-                    sign = -1 if ch == '-' else 1
-                    i += 1
-                    while i < n and s[i] == ' ':
-                        i += 1
-                    start = i
-                    while i < n and s[i].isdigit():
-                        i += 1
-                    num = int(s[start:i]) * sign
-                    deNum.append(num)
-                    if opFlag == 1:
-                        opFlag = 0
-                        a = deNum.pop()
-                        b = deNum.pop()
-                        op = deOp.pop()
-                        deNum.append(calc(b, a, op))
-                else:
-                    deOp.append(ch)
-                    i += 1
-            elif ch == ' ':
-                i += 1
-            else:
-                start = i
-                while i < n and s[i].isdigit():
-                    i += 1
-                num = int(s[start:i])
-                deNum.append(num)
-                if opFlag == 1:
-                    opFlag = 0
-                    a = deNum.pop()
-                    b = deNum.pop()
-                    op = deOp.pop()
-                    deNum.append(calc(b, a, op))
-
-        while deOp:
-            a = deNum.popleft()
-            b = deNum.popleft()
-            op = deOp.popleft()
-            deNum.appendleft(calc(a, b, op))
-
-        return deNum[0] if deNum else 0
+                    pass
+                match sign:
+                    case '+':
+                        ans += last
+                        last = num
+                    case '-':
+                        ans += last
+                        last = -num
+                    case '*':
+                        last = last * num
+                    case '/':
+                        last = int(last / num)
+                sign = ch
+                num = 0
+        return ans + last
