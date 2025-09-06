ac4e9139253d59ef05820682421c17d2587580a0
Chaoyi Jiang <jiang.chao@northeastern.edu>
2025-09-05 23:29:26 -0400

    LC-sort-array-by-parity [Runtime: 0 ms; Memory: 18.7 MB]

--- two-pointers/sort-array-by-parity.py
+++ two-pointers/sort-array-by-parity.py
@@ -4,9 +4,13 @@ class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
         i, j = 0, len(nums) - 1
         while i < j:
-            if nums[i] % 2 == 0:
+            while i < j and nums[i] % 2 == 0:
                 i += 1
-            else:
+            while i < j and nums[j] % 2 == 1:
+                j -= 1
+            if i < j:
                 nums[i], nums[j] = nums[j], nums[i]
+                i += 1
                 j -= 1
         return nums
+
