75254bb8144e91797079c3df540dc264948d8f90
Chaoyi Jiang <jiang.chao@northeastern.edu>
2025-09-08 15:49:01 -0400

    LC-gas-station [Runtime: 19 ms; Memory: 23.2 MB]

--- greedy/gas-station.py
+++ greedy/gas-station.py
@@ -5,7 +5,6 @@ class Solution:
         total = 0
         tank = 0
         start = 0
-
         for i in range(len(gas)):
             gain = gas[i] - cost[i]
             total += gain
@@ -13,21 +12,4 @@ class Solution:
             if tank < 0:
                 start = i + 1
                 tank = 0
-
-        return start if total >= 0 and start < len(gas) else -1
-
-    def canCompleteCircuit_prefix(self, gas: List[int], cost: List[int]) -> int:
-        total = 0
-        min_prefix = 10**18
-        min_idx = -1
-        prefix = 0
-        for i in range(len(gas)):
-            gain = gas[i] - cost[i]
-            total += gain
-            prefix += gain
-            if prefix < min_prefix:
-                min_prefix = prefix
-                min_idx = i
-        if total < 0:
-            return -1
-        return (min_idx + 1) % len(gas)
+        return start if total >= 0 else -1
