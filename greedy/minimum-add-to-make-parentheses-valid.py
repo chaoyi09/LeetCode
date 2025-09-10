1d0c610ad3c0e869030845ead4c35770593e3954
Chaoyi Jiang <jiang.chao@northeastern.edu>
2025-09-10 15:44:15 -0400

    LC-minimum-add-to-make-parentheses-valid [Runtime: 0 ms; Memory: 17.7 MB]

--- greedy/minimum-add-to-make-parentheses-valid.py
+++ greedy/minimum-add-to-make-parentheses-valid.py
@@ -1,11 +1,14 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
-        ans = bal = 0
+        open_cnt = 0
+        res = 0
+
         for c in s:
-            if c == '(':
-                bal += 1
-            elif bal:
-                bal -= 1
+            if c == "(":
+                open_cnt += 1
             else:
-                ans += 1
-        return ans + bal
+                if open_cnt == 0:
+                    res += 1
+                open_cnt = max(open_cnt - 1, 0)
+
+        return res + open_cnt
