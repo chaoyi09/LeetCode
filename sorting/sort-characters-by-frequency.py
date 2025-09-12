603e11f02fd0b880cf5d5a6a08aa16306ac08ae2
Chaoyi Jiang <jiang.chao@northeastern.edu>
2025-09-12 10:20:42 -0400

    LC-sort-characters-by-frequency [Runtime: 11 ms; Memory: 19 MB]

--- sorting/sort-characters-by-frequency.py
+++ sorting/sort-characters-by-frequency.py
@@ -1,18 +1,15 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
-        if not s:
-            return ""
-
         cnt = Counter(s)
-        n = len(s)
-        buckets: List[List[str]] = [[] for _ in range(n + 1)]
+        buckets = defaultdict(list)
+        maxf = 0
         for ch, f in cnt.items():
             buckets[f].append(ch)
+            if f > maxf:
+                maxf = f
 
         res = []
-        for f in range(n, 0, -1):
-            if not buckets[f]:
-                continue
-            for ch in buckets[f]:
+        for f in range(maxf, 0, -1):
+            for ch in buckets.get(f, ()):
                 res.append(ch * f)
         return "".join(res)
