607daa9a2306abf5839c38ca86bd26eee4fe5bbb
Chaoyi Jiang <jiang.chao@northeastern.edu>
2025-10-03 22:26:32 -0400

    LC-valid-word-abbreviation [Runtime: 3 ms; Memory: 17.7 MB]

--- string/valid-word-abbreviation.py
+++ string/valid-word-abbreviation.py
@@ -4,20 +4,28 @@ class Solution:
         n, m = len(word), len(abbr)
 
         while j < m:
-            if abbr[j].isdigit():
-                if abbr[j] == '0':
+            ch = abbr[j]
+
+            if ch.isalpha():
+
+                if i >= n or word[i] != ch:
                     return False
-                num = 0
-                while j < m and abbr[j].isdigit():
-                    num = num * 10 + (ord(abbr[j]) - ord('0'))
-                    j += 1
-                i += num
-                if i > n:
-                    return False
-            else:
-                if i >= n or word[i] != abbr[j]:
-                    return False
+
                 i += 1
                 j += 1
 
+            else:
+                if ch == '0':
+                    return False
+
+                num = 0
+
+                while j < m and abbr[j].isdigit():
+                    num = num * 10 + (ord(abbr[j]) - ord('0'))
+                    j += 1
+
+                i += num
+                if i > n:
+                    return False
+                
         return i == n
