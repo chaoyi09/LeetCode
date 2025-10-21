ab248c28dc10384cf66a6888e4ead177cbfb0130
Chaoyi Jiang <jiang.chao@northeastern.edu>
2025-10-21 16:30:50 -0400

    LC-binary-tree-level-order-traversal [Runtime: 3 ms; Memory: 18.7 MB]

--- binary-tree/binary-tree-level-order-traversal.py
+++ binary-tree/binary-tree-level-order-traversal.py
@@ -1,29 +1,26 @@
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root:
-            return []
+        res = []
 
-        result = []
-        queue = deque([root])
+        q = collections.deque()
+        q.append(root)
 
-        while queue:
-            level_size = len(queue)
-            level_values = []
+        while q:
+            qLen = len(q)
+            level = []
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                res.append(level)
 
-            for _ in range(level_size):
-                node = queue.popleft()
-                level_values.append(node.val)
-
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-
-            result.append(level_values)
-        return result        
+        return res
