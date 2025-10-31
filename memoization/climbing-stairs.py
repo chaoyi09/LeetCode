bf74bd028a375a9fbf8584dba6e89bab9ffa2583
Chaoyi Jiang <jiang.chao@northeastern.edu>
2025-10-30 21:14:20 -0400

    LC-climbing-stairs [Runtime: 0 ms; Memory: 17.7 MB]

--- memoization/climbing-stairs.py
+++ memoization/climbing-stairs.py
@@ -1,9 +1,11 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        sqrt5 = math.sqrt(5)
-        phi = (1 + sqrt5) / 2
-        psi = (1 - sqrt5) / 2
+        if n <= 2:
+            return n
 
-        n += 1
+        prev2, prev1 = 1, 2
+        for i in range(3, n + 1):
+            current = prev1 + prev2
+            prev2, prev1 = prev1, current
 
-        return round((phi**n - psi**n) / sqrt5)
+        return prev1
