12fb5deb70e42341edac1927bd48378e02d7666f
Chaoyi Jiang <jiang.chao@northeastern.edu>
2026-03-09 23:16:46 -0400

    LC-find-kth-bit-in-nth-binary-string [Runtime: 0 ms; Memory: 19.3 MB]

---
+++ string/find-kth-bit-in-nth-binary-string.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findKthBit(self, n: int, k: int) -> str:
+        if n == 1:
+            return '0'
+
+        mid = 2 ** (n-1)
+
+        if k == mid:
+            return '1'
+
+        elif k < mid:
+            return self.findKthBit(n - 1, k)
+
+        else:
+            mirror = 2 ** n - k
+            c = self.findKthBit(n - 1, mirror)
+            return '1' if c == '0' else '0'
